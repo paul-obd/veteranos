cc7f350955d06e4f9e68214a45f20a5557b4f01a
paul.obd <paul.obd@hotmail.com>
2021-03-06 19:42:21 +0200

    first commit veteranos tester

---
+++ food.js
@@ -0,0 +1,40 @@
+// function fetchit(){
+
+    
+
+      
+//     fetch('https://paul342021.herokuapp.com/')
+//   .then(response => response.json())
+//   .then(json => {
+//      var value = document.getElementById('food').value;
+//       var jsonel = json[i];
+//       var priceel = jsonel.price;
+
+     
+//   })
+//   document.getElementById('price-value').innerHTML = priceel;
+// }
+
+function fetchit() {
+
+    var ul = document.getElementById('ul');
+  
+  fetch('https://paul342021.herokuapp.com/')
+    .then(response => response.json())
+    .then(json => {
+      for (let i = 0; i < json.length; i++) {
+        var jsonel = json[i];
+        var li = document.createElement('li');
+          li.innerHTML = (jsonel.name + ":" + " " + jsonel.price) ;
+          ul.appendChild(li);
+  
+        
+        
+  
+        
+      }
+      
+    }
+      
+    )}
+    fetchit();
---
+++ vaccines.js
@@ -0,0 +1,23 @@
+function fetchit() {
+
+    var ul = document.getElementById('ul');
+  
+  fetch('https://paul342021.herokuapp.com/vaccines')
+    .then(response => response.json())
+    .then(json => {
+      for (let i = 0; i < json.length; i++) {
+        var jsonel = json[i];
+        var li = document.createElement('li');
+          li.innerHTML = (jsonel.name + ":" + " " + jsonel.price) ;
+          ul.appendChild(li);
+  
+        
+        
+  
+        
+      }
+      
+    }
+      
+    )}
+    fetchit();
