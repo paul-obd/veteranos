af5e5784cf4e6445e6a207f8a979bad2595fe6fc
paul.obd <paul.obd@hotmail.com>
2021-03-07 14:26:03 +0200

    zedna rate l dollar

--- food.js
+++ food.js
@@ -15,17 +15,27 @@
 //   document.getElementById('price-value').innerHTML = priceel;
 // }
 
+
+
+
+
 function fetchit() {
 
     var ul = document.getElementById('ul');
+    var rate = document.getElementById('dollar-rate').value;
+    
   
   fetch('https://paul342021.herokuapp.com/')
     .then(response => response.json())
     .then(json => {
       for (let i = 0; i < json.length; i++) {
         var jsonel = json[i];
+        
+        
         var li = document.createElement('li');
-          li.innerHTML = (jsonel.name + ":" + " " + jsonel.price) ;
+          li.innerHTML = (jsonel.name + ":" + " " + jsonel.price + jsonel.dollar + "        " + (rate*jsonel.price) + "L.L." );
+      
+          
           ul.appendChild(li);
   
         
@@ -37,4 +47,7 @@ function fetchit() {
     }
       
     )}
-    fetchit();
+    // fetchit();
+
+
+  
--- pharmacie.js
+++ pharmacie.js
@@ -1,6 +1,7 @@
 function fetchit() {
 
     var ul = document.getElementById('ul');
+    var rate = document.getElementById('dollar-rate').value;
   
   fetch('https://paul342021.herokuapp.com/pharmacie')
     .then(response => response.json())
@@ -8,7 +9,7 @@ function fetchit() {
       for (let i = 0; i < json.length; i++) {
         var jsonel = json[i];
         var li = document.createElement('li');
-          li.innerHTML = (jsonel.name + ":" + " " + jsonel.price) ;
+          li.innerHTML = (jsonel.name + ":" + " " + jsonel.price + jsonel.dollar + "        " + (rate*jsonel.price) + "L.L.") ;
           ul.appendChild(li);
   
         
@@ -20,4 +21,3 @@ function fetchit() {
     }
       
     )}
-    fetchit();
